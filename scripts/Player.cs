5b3c9fd0cda6c703840c0a44874e2a9141c18de9
NicoCodesCode <nicolasrmz08@gmail.com>
2025-04-26 11:48:51 -0500

    add collisions to the player

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,28 +3,12 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	private int _speed = 400;
-	
-	public override void _Ready()
-	{
-	}
+	private float _speed = 400f;
 
-	public override void _Process(double delta)
-	{
-		Vector2 position = Position;
-		int direction = 0;
-
-		if (Input.IsActionPressed("MoveUp"))
-		{
-			direction = -1;
-		}
-
-		if (Input.IsActionPressed("MoveDown"))
-		{
-			direction = 1;
-		}
-
-		position.Y += _speed * (float)delta * direction;
-		Position = position;
-	}
+    public override void _PhysicsProcess(double delta)
+    {
+        float direction = Input.GetAxis("MoveUp", "MoveDown");
+        Velocity = new Vector2(0, direction * _speed);
+		MoveAndSlide();
+    }
 }
