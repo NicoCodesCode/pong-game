1373d686196eb012e00cd92223e3d0a0301bce44
NicoCodesCode <nicolasrmz08@gmail.com>
2025-04-26 07:35:19 -0500

    use 4 spaces for indentation

--- scripts/Ball.cs
+++ scripts/Ball.cs
@@ -5,13 +5,13 @@ public partial class Ball : CharacterBody2D
 {
 	private Vector2 _velocity = new Vector2(400, 400);
 
-  public override void _PhysicsProcess(double delta)
-  {
+    public override void _PhysicsProcess(double delta)
+    {
 		KinematicCollision2D collision = MoveAndCollide(_velocity * (float)delta);
 
 		if (collision != null)
 		{
 			_velocity = _velocity.Bounce(collision.GetNormal());
 		}
-  }
+    }
 }
