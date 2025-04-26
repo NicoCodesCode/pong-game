e31768f2d008a01cb63e7db7fcc1f33605e6e7b4
NicoCodesCode <nicolasrmz08@gmail.com>
2025-04-26 09:36:58 -0500

    reset ball to center after scoring

--- scripts/Ball.cs
+++ scripts/Ball.cs
@@ -5,6 +5,18 @@ public partial class Ball : CharacterBody2D
 {
 	private Vector2 _velocity = new Vector2(500, 500);
 
+	[Export]
+	public Area2D PlayerScoreArea;
+
+	[Export]
+	public Area2D ComputerScoreArea;
+
+	public override void _Ready()
+	{
+		PlayerScoreArea.BodyEntered += OnBodyEntered;
+		ComputerScoreArea.BodyEntered += OnBodyEntered;
+	}
+
     public override void _PhysicsProcess(double delta)
     {
 		KinematicCollision2D collision = MoveAndCollide(_velocity * (float)delta);
@@ -14,4 +26,9 @@ public partial class Ball : CharacterBody2D
 			_velocity = _velocity.Bounce(collision.GetNormal());
 		}
     }
+
+	private void OnBodyEntered(Node2D body)
+	{
+		Position = new Vector2(575, 322);
+	}
 }
